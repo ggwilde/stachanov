0d222a97d5b401234936836028f287fdd2ce67e0
ggwilde <ggwilde@protonmail.com>
2017-09-14 18:08:53 +0200

    ADD/REFACTOR TreeStorage / BlockStorage
    * added a new storage trait TreeStorage
    * moved block storage handling to a parent trait
      for ChainStorage and TreeStorage

--- src/blockchain/traits.rs
+++ src/blockchain/traits.rs
@@ -30,17 +30,45 @@ pub trait Hashable {
     fn to_sha3_hash(&self) -> [u8; 32];
 }
 
-/// The `ChainStorage` trait must be implemented by all storage backends.
-/// We allow for different backends (redis, postgres, etc) as not
-/// every storage type fits for every service built around the core.
 
-pub trait ChainStorage{
+/// `BlockStorage` is the parent trait for `ChainStorage`
+/// and `TreeStorage`. It defines an interface for adding
+/// blocks as well as for retrieving blocks and transactions
+
+pub trait BlockStorage{
 
     /// Fetches a block identified by its unique block id
     /// * `block_id`: The block identifier (equivalent to the
     ///               the sha3 hash of the block header)
 
     fn get_block(&self, block_id: BlockId) -> Option<Block>;
+
+    /// Append a block to the storage *without* checking its
+    /// validity. Will return an IdCollisionError if there
+    /// already is a block with the same block id.
+    /// * `block`: The verified block
+
+    fn append_verified_block(&mut self, block: Block)
+            -> Result<(), IdCollisionError>;
+
+    /// Fetches a transaction identifier by its id
+    /// * `tx_id`: the transaction id
+
+    fn get_transaction(&self, tx_id: TxId)
+            -> Option<Transaction>;
+
+    /// Removes all data from the storage
+
+    fn reset(&mut self);
+
+}
+
+/// The `ChainStorage` trait must be implemented by all storage
+/// backends saving the chain consensus. In contrast to `TreeStorage`,
+/// all blocks in `ChainStorage` (except the last one) have a distinct
+/// subsequent block, so we can implement iterator primitives.
+
+pub trait ChainStorage: BlockStorage {
 
     /// Fetches the block *after* the supplied block id. This
     /// is mainly an interface for block iteration.
@@ -63,26 +91,16 @@ pub trait ChainStorage{
 
     fn get_tail_block(&self) -> Option<Block>;
 
-    /// Append a block to the chain *without* checking its
-    /// validity. Will return an IdCollisionError if there
-    /// already is a block with the same block id.
-    /// * `block`: The verified block
-
-    fn append_verified_block(&mut self, block: Block) -> Result<(), IdCollisionError>;
-
-    /// Fetches a transaction identifier by its id
-    /// * `tx_id`: the transaction id
-
-    fn get_transaction(&self, tx_id: TxId) -> Option<Transaction>;
-
     /// Fetches the `TxState` of a transaction
     /// * `tx_id`: the transaction id
 
     fn get_transaction_state(&self, tx_id: TxId) -> Option<TxState>;
 
     /// Updates the `TxState` of a transaction.
+    ///
     /// Returns a TxProgError when tx_id points to
-    /// a non-existent transaction
+    /// a non-existent transaction or if one of
+    /// the relationships has invalid data.
     ///
     /// * `tx_id`: the transaction id
     /// * `tx_state`: the appropriate transaction state
@@ -91,8 +109,53 @@ pub trait ChainStorage{
                              tx_id: TxId,
                              tx_state: TxState) -> Result<(), TxProgError>;
 
-    /// Removes all data from the storage
+}
 
-    fn reset(&mut self);
+/// The `TreeStorage` trait must be implemented by all storage
+/// backends saving the fuzzy tail of the blockchain.
+
+pub trait TreeStorage: BlockStorage {
+
+    /// Fetches the `TxState` of a transaction
+    /// at a distinct position in the tree.
+    ///
+    /// * `branch_tail_id`: A block id identifying
+    ///         a branch in the tree
+    /// * `tx_id`: the transaction id
+
+    //         .---.      .---.    If you look at the tree on the left
+    //    .--o | B | o--o | E |    you will notice, that the state of
+    //    o    '---'      '---'    transactions in A is ambigous. It
+    //  .---.      .---.           depends on wether we look at the
+    //  | A | o--o | C |           branch A-B-E, A-C or A-D.
+    //  '---'      '---'           Therefore we need to define for
+    //    o    .---.               which branch we set the state.
+    //    '--o | D |               The branch is identified by its
+    //         '---'               tail, meaning that by setting
+    //                             branch_tail_id to E, we get the
+    //                             branch A-B-E, by setting it to C,
+    //                             the branch A-C, etc.
+
+
+    fn get_transaction_state(&self,
+                             branch_tail_id: BlockId,
+                             tx_id: TxId) -> Option<TxState>;
+
+    /// Updates the `TxState` of a transaction
+    /// at a distinct position in the tree.
+    ///
+    /// Returns a TxProgError when tx_id points to
+    /// a non-existent transaction or if one of
+    /// the relationships has invalid data.
+    ///
+    /// * `branch_tail_id`: A block id identifying
+    ///         a branch in the tree
+    /// * `tx_id`: the transaction id
+    /// * `tx_state`: the appropriate transaction state
+
+    fn set_transaction_state(&mut self,
+                             branch_tail_id: BlockId,
+                             tx_id: TxId,
+                             tx_state: TxState) -> Result<(), TxProgError>;
 
 }
